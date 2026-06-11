80a2f666e6e4fc09f68ab41ff7ce66f8be4242f6
laikdaniil-gif <laikdaniil@gmail.com>
2026-06-11 16:19:01 +0300

    Delete client/client directory

--- client/client/src/api/materials.js
+++
@@ -1,5 +0,0 @@
-import api from './axiosInstance';
-
-export const fetchMaterials = () => api.get('/material');
-export const createMaterial = (name) => api.post('/material', { name });
-export const deleteMaterial = (id) => api.delete(`/material/${id}`);
--- client/client/src/utils/decodeToken.js
+++
@@ -1,7 +0,0 @@
-export const decodeToken = (token) => {
-  try {
-    return JSON.parse(atob(token.split('.')[1]));
-  } catch {
-    return null;
-  }
-};
